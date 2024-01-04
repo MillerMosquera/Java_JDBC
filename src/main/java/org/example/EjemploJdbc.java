f94639f2d6c4f9e5169175f07de15265300a7a44
Miller Mosquera <millerpalacios16@gmail.com>
2024-01-04 11:54:16 -0500

    create repository and crud example jdbc

--- src/main/java/org/example/EjemploJdbc.java
+++ src/main/java/org/example/EjemploJdbc.java
@@ -1,26 +1,53 @@
 package org.example;
 
+import org.example.models.Producto;
+import org.example.repositorio.IRepositorio;
+import org.example.repositorio.Implements.ProductoRepositorioImpl;
 import org.example.util.ConexionBaseDatos;
 
 import java.sql.*;
+import java.util.Date;
 
 public class EjemploJdbc {
     public static void main(String[] args) {
 
+        try (Connection conn = ConexionBaseDatos.getInstance()) {
 
-        try (Connection conn = ConexionBaseDatos.getInstance();
-             Statement statement = conn.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM productos")) {
+            IRepositorio<Producto> repositorio = new ProductoRepositorioImpl();
+            System.out.println("============= Listar =============");
+            repositorio.listar().forEach(System.out::println);
 
-            while (resultSet.next()) {
-                System.out.print(resultSet.getInt("id"));
-                System.out.print(" | ");
-                System.out.print(resultSet.getString("nombre"));
-                System.out.print(" | ");
-                System.out.print(resultSet.getInt("precio"));
-                System.out.print(" | ");
-                System.out.print(resultSet.getDate("fecha_registro"));
-            }
+            System.out.println("============= Listar por Id =============");
+            System.out.println(repositorio.porId(2L));
+
+            /*System.out.println("============= Insertar =============");
+            Producto producto = new Producto();
+            producto.setNombre("Teclado");
+            producto.setPrecio(150);
+            producto.setFecha_registro(new Date());
+            repositorio.guardar(producto);
+            System.out.println("Producto guardado con éxito");
+            repositorio.listar().forEach(System.out::println);
+
+             */
+
+            /*
+            Producto producto = new Producto();
+            System.out.println("============= Update =============");
+            producto.setId(4L);
+            producto.setNombre("Teclado Mecánico");
+            producto.setPrecio(700);
+            repositorio.guardar(producto);
+            System.out.println("Se actualizó con éxito");
+            repositorio.listar().forEach(System.out::println);
+             */
+
+            System.out.println("============= Eliminar =============");
+
+            repositorio.eliminar(3L);
+            System.out.println("Se eliminó con éxito");
+            repositorio.listar().forEach(System.out::println);
+
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();

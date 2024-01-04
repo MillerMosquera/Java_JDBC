5d0740b5c771b3dfb83d7c33c56ef0ad3ece85d8
Miller Mosquera <millerpalacios16@gmail.com>
2024-01-04 09:31:49 -0500

    add singleton and update first commit

--- src/main/java/org/example/EjemploJdbc.java
+++ src/main/java/org/example/EjemploJdbc.java
@@ -1,16 +1,14 @@
 package org.example;
 
+import org.example.util.ConexionBaseDatos;
+
 import java.sql.*;
 
 public class EjemploJdbc {
     public static void main(String[] args) {
 
 
-        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=UTC-5";
-        String username = "root";
-        String password = "2404";
-
-        try (Connection conn = DriverManager.getConnection(url, username, password);
+        try (Connection conn = ConexionBaseDatos.getInstance();
              Statement statement = conn.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT * FROM productos")) {
 
